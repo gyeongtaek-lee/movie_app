7ff2d85cfeafea6b5ff11ab4f17b60aee1bbda20
131084 <131084@ssg.com>
2020-11-26 15:16:42 +0900

    IE Polyfill EventSource..

--- src/App.js
+++ src/App.js
@@ -1,56 +1,96 @@
-import React, {useEffect, useState} from "react";
-import axios from "axios";
-const http = require('http');
-const KeepAliveAgent = require('keep-alive-agent');
-
-
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-// module.exports = function (app) {
-//     app.use(
-//         '/api',
-//         createProxyMiddleware({
-//             target: 'http://localhost:8080',
-//             changeOrigin: true
-//         })
-//     );
-// };
-
-const getOptions = {
-    // hostname: '127.0.0.1',
-    // port: 8080,
-    path: '/kafka-messages',
-    agent: KeepAliveAgent,
-};
-
-
-const getMessage = () => {
-    http.get(getOptions, function(_response)
-    {
-        // console.log(_response);
-        // _response.pipe(process.stdout);
-        _response.on('data', (data)=>{
-            const _data = Buffer.concat([data]).toString().trim();
-            console.log(_data);
-        }).on('end', () =>{
-
-        })
-    });
-}
-
+import React, {useEffect, useRef, useState} from "react";
+import './chat.css';
+import './polyfill/EventSource.js';
 
 const App = () => {
 
-    // getMessage();
+    const [message, sendMsg] = useState("");
 
-    axios.get('/kafka-messages')  // http://localhost:5000 안 써줘도 괜찮다.
-        .then(res=> console.log(res.data))
-        .catch()
+    const msgArea = useRef(null);
+
+    const useEventSource = (url) => {
+
+        const [data, updateData] = useState("");
+        const msg = useRef("");
+
+        useEffect(() => {
+
+            const source = new EventSource(url, {withCredentials: false});
+
+            source.onmessage = (ev) => {
+                msg.current += ev.data+"\n";
+                updateData(msg.current);
+
+                msgArea.current.scrollIntoView({ behavior: "smooth" });
+                msgArea.current.scrollTop = msgArea.current.scrollHeight - msgArea.current.clientHeight;
+            }
+
+            source.onopen = (e) => {
+                console.log(e);
+            }
+
+            source.onerror = error => {
+                console.log(error);
+
+                if (this.readyState === EventSource.CONNECTING) {
+                    console.log(`Reconnecting (readyState=${this.readyState})...`)
+                }
+                else {
+                    console.log("Error has occured.");
+                }
+
+            }
+
+
+
+        }, []);
+
+        return data;
+    }
+
+    const data = useEventSource('http://10.149.181.149:8080/kafka-messages');
+
+    if (!data) {
+        return <div />;
+    }
+
+    const press = (e) => {
+        const d = new Date();
+
+        let id = '';
+        let ap = d.getHours() < 12 ? "AM" : "PM";
+        let h = (d.getHours() % 12) ? (d.getHours() % 12) : 12;
+
+        console.log(e.key);
+
+        if (e.key === 'Enter' && e.target.value) {
+
+            // handleCreate();
+
+            fetch(`http://10.149.181.149:8080/kafka-send-messages?msg=${ap} ${h}:${d.getMinutes()}   ${id}: ${e.target.value}`, {
+                method: 'GET'
+            }).then(r => console.log('complete'));
+            //
+            e.target.value = "";
+
+        }
+    }
 
     return (
 
         <div>
-            <h1>hi~~</h1>
+            ID : <input type="text" id="uuid"/>
+
+            <div id="msgArea" ref={msgArea} className="scroll_box">
+                {data.split('\n').map((line, idx) => {
+                    return (<span key={idx}>{line}<br/></span>)
+                })}
+            </div>
+
+            <div className="textbox">
+                <label>message : </label>
+                <input type="text" name="ex_input" onKeyPress={press} />
+            </div>
         </div>
 
     );
